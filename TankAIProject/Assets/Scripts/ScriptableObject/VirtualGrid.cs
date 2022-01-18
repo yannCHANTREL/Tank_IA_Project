5e784fccb6e9160b4e32e932052ebcdf8a1a3afc
Christophe Fracheboud <christophefrache@hotmail.com>
2022-01-18 12:54:16 +0100

    Create the Grid asset

--- TankAIProject/Assets/Scripts/ScriptableObject/VirtualGrid.cs
+++ TankAIProject/Assets/Scripts/ScriptableObject/VirtualGrid.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Virtual Grid")]
 public class VirtualGrid : ScriptableObject
 {
     public bool[,] Grid;
