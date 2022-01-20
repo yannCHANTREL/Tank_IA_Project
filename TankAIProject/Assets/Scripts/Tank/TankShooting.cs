b39de7f8b8a45bea30d87f5d98fff159376fdc83
jducamp <jeanducamp@hotmail.fr>
2022-01-20 12:55:31 +0100

    Revert "move and turn command"
    This reverts commit 3ba056dcced21ba9dee2a7519235b09ceb73dd6f.

--- TankAIProject/Assets/Scripts/Tank/TankShooting.cs
+++ TankAIProject/Assets/Scripts/Tank/TankShooting.cs
@@ -36,7 +36,12 @@ namespace Complete
 
         private void Update ()
         {
-            
+            // Otherwise, if the fire button has just started being pressed...
+            /*if (Input.GetButton(m_FireButton) && !m_Reloading)
+            {
+                // ... reset the fired flag and reset the launch force.
+                StartCoroutine(Fire());
+            }*/
         }
 
         public void OnFireCommand()
