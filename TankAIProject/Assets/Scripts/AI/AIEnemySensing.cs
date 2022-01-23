a6c7ab2f4d6b9e68c64f42c7f6155c190ccb6ea9
jducamp <jeanducamp@hotmail.fr>
2022-01-23 22:35:25 +0100

    AI pos prediction improve

--- TankAIProject/Assets/Scripts/AI/AIEnemySensing.cs
+++ TankAIProject/Assets/Scripts/AI/AIEnemySensing.cs
@@ -7,36 +7,79 @@ public class AIEnemySensing : MonoBehaviour
 {
     public GameObject m_TankTarget;
     public Vector3ListVariable m_TankTargetPos;
-    public Vector3ListVariable m_TankTargetSpeed;
-    public Vector3ListVariable m_TankTargetEstimatedPos;
     public Vector3ListVariable m_TankTargetLastPos;
+    public Vector3ListVariable m_TankTargetSpeed;
+    public Vector3ListVariable m_TankTargetLastSpeed;
+    public Vector3ListVariable m_TankTargetEstimatedPos;
+    public FloatListVariable m_TankTargetTimeToReachEstimatedPos;
     public TankIndexManager m_TankIndexManager;
 
     public float m_ShellSpeed = 20;
+    public bool m_StraightEstimation;
+    public int m_MaxEstimationStep = 1000;
 
     public void FixedUpdate()
     {
         if (m_TankTarget)
         {
+            Vector3 tankTargetEstimatedPos;
+            float dt = Time.fixedDeltaTime;
             Vector3 tankTargetLastPos = m_TankTargetPos.m_Values[m_TankIndexManager.m_TankIndex];
             Vector3 tankTargetPos = m_TankTarget.transform.position;
-            Vector3 tankTargetVelocity = (tankTargetPos - tankTargetLastPos) / Time.fixedDeltaTime;
-            Vector3 tankTargetDistance = tankTargetPos - transform.position;
-            float tankTargetVelocityMagnitude = tankTargetVelocity.magnitude;
-            float tankTargetDistanceMagnitude = tankTargetDistance.magnitude;
-            float tankTargetAngleDir = Vector3.Angle(tankTargetDistance, tankTargetVelocity);
+            Vector3 tankTargetDeltaPos = tankTargetPos - tankTargetLastPos;
+            Vector3 tankTargetLastSpeed = m_TankTargetSpeed.m_Values[m_TankIndexManager.m_TankIndex];
+            Vector3 tankTargetSpeed = tankTargetDeltaPos / dt;
+            Vector3 tankPos = transform.position;
+            Vector3 tankTargetDistance = tankTargetPos - tankPos;
+            float tankTargetVelocityMagnitude = tankTargetSpeed.magnitude;
+            float tankDistanceMagnitude = tankTargetDistance.magnitude;
+            float tankTargetAngleDir = Vector3.SignedAngle(tankTargetDistance, tankTargetSpeed, Vector3.up);
+            float tankTargetTimeToReachEstimatedPos;
 
-            float squareSpeedDiff = Mathf.Pow(m_ShellSpeed, 2f) - Mathf.Pow(tankTargetVelocityMagnitude, 2f);
-            float squareTargetDistance = Mathf.Pow(tankTargetDistanceMagnitude, 2f);
-            float speedDistanceCosAngle = tankTargetVelocityMagnitude * tankTargetDistanceMagnitude * Mathf.Cos(tankTargetAngleDir);
-            float speedDistanceCosAngleDivSquareSpeedDiff = speedDistanceCosAngle / squareSpeedDiff;
-            float shellTravelTime = squareSpeedDiff == 0 ? squareTargetDistance / 2f / speedDistanceCosAngle : Mathf.Sqrt(Mathf.Pow(speedDistanceCosAngleDivSquareSpeedDiff, 2f) + squareTargetDistance / squareSpeedDiff) - speedDistanceCosAngleDivSquareSpeedDiff;
-            Vector3 tankTargetEstimatedPos = tankTargetPos + tankTargetVelocity * shellTravelTime;
+            if (m_StraightEstimation)
+            {
+                float squareSpeedDiff = Mathf.Pow(m_ShellSpeed, 2f) - Mathf.Pow(tankTargetVelocityMagnitude, 2f);
+                float squareTargetDistance = Mathf.Pow(tankDistanceMagnitude, 2f);
+                float speedDistanceCosAngle = tankTargetVelocityMagnitude * tankDistanceMagnitude * Mathf.Cos(tankTargetAngleDir);
+                float speedDistanceCosAngleDivSquareSpeedDiff = speedDistanceCosAngle / squareSpeedDiff;
+                float shellTravelTime = squareSpeedDiff == 0 ? squareTargetDistance / 2f / speedDistanceCosAngle : Mathf.Sqrt(Mathf.Pow(speedDistanceCosAngleDivSquareSpeedDiff, 2f) + squareTargetDistance / squareSpeedDiff) - speedDistanceCosAngleDivSquareSpeedDiff;
+                tankTargetEstimatedPos = tankTargetPos + tankTargetSpeed * shellTravelTime;
+                tankTargetTimeToReachEstimatedPos = shellTravelTime;
+            }
+            else
+            {
+                float currentTankTargetEstimatedError;
+                float currentTankTargetTravelTime = 0;
+                float TankTargetStepTravelTime = tankTargetDeltaPos.magnitude / m_ShellSpeed;
+                float shellTravelTime;
+                float bestTankTargetTimeEstimatedError = float.MaxValue;
+                Vector3 currentTankTargetEstimatedPos = tankTargetPos;
+                Vector3 TankTargetStepEstimatedDeltaPos = tankTargetDeltaPos;
+                tankTargetEstimatedPos = currentTankTargetEstimatedPos;
 
-            m_TankTargetPos.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetPos;
-            m_TankTargetSpeed.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetVelocity;
-            m_TankTargetEstimatedPos.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetEstimatedPos;
+                for (int i = 0; i < m_MaxEstimationStep; i++)
+                {
+                    TankTargetStepEstimatedDeltaPos = Quaternion.AngleAxis(tankTargetAngleDir, Vector3.up) * TankTargetStepEstimatedDeltaPos;
+                    tankTargetEstimatedPos += TankTargetStepEstimatedDeltaPos;
+                    currentTankTargetTravelTime += TankTargetStepTravelTime;
+                    shellTravelTime = (tankTargetEstimatedPos - tankPos).magnitude / m_ShellSpeed;
+                    currentTankTargetEstimatedError = Mathf.Abs(currentTankTargetTravelTime - shellTravelTime);
+                    if (currentTankTargetEstimatedError < bestTankTargetTimeEstimatedError)
+                    {
+                        bestTankTargetTimeEstimatedError = currentTankTargetEstimatedError;
+                    }
+                    else { break; }
+                }
+
+                tankTargetTimeToReachEstimatedPos = currentTankTargetTravelTime - TankTargetStepTravelTime;
+            }
+
             m_TankTargetLastPos.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetLastPos;
+            m_TankTargetPos.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetPos;
+            m_TankTargetLastSpeed.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetLastSpeed;
+            m_TankTargetSpeed.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetSpeed;
+            m_TankTargetEstimatedPos.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetEstimatedPos;
+            m_TankTargetTimeToReachEstimatedPos.m_Values[m_TankIndexManager.m_TankIndex] = tankTargetTimeToReachEstimatedPos;
         }
     }
 }
--- TankAIProject/Assets/Scripts/AI/AIShoot.cs
+++ TankAIProject/Assets/Scripts/AI/AIShoot.cs
@@ -6,10 +6,12 @@ public class AIShoot : MonoBehaviour
 {
     public Vector3ListVariable m_TargetPos;
     public Vector3ListVariable m_TargetEstimatedPos;
+    public FloatListVariable m_TargetTimeToReachEstimatedPos;
     public bool m_PredictTargetMovement = true;
     public float m_FireRange = 13;
     public float m_AngularTolerance = 5f;
     public float m_RadiusTolerance = 0.5f;
+    public float m_ShellSpeed = 20;
     public TankIndexManager m_TankIndexManager;
     public TankEvent m_FireCommand;
 
@@ -27,7 +29,7 @@ public class AIShoot : MonoBehaviour
         Vector3 distance = targetPos - tankPos;
         float angleToTarget = Vector3.Angle(distance, tankForward);
         
-        if (distance.magnitude < m_FireRange + m_RadiusTolerance && angleToTarget < m_AngularTolerance)
+        if (distance.magnitude < m_FireRange + m_RadiusTolerance && angleToTarget < m_AngularTolerance && (distance - tankForward * m_ShellSpeed * m_TargetTimeToReachEstimatedPos.m_Values[m_TankIndexManager.m_TankIndex]).magnitude < m_RadiusTolerance)
         {
             m_FireCommand.Raise(m_TankIndexManager.m_TankIndex);
         }
