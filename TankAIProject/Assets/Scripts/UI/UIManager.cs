37acce5af148bf5e6bc5891b121784022835598c
Christophe Fracheboud <christophefrache@hotmail.com>
2022-01-28 13:05:34 +0100

    Advance menu UI

---
+++ TankAIProject/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject m_TitleScreen;
+    public GameObject m_OptionScreen;
+
+    private void Start()
+    {
+        GoToTitleScreen();
+    }
+
+    public void GoToTitleScreen()
+    {
+        m_TitleScreen.SetActive(true);
+        m_OptionScreen.SetActive(false);
+    }
+    
+    public void GoToOptionScreen()
+    {
+        m_TitleScreen.SetActive(false);
+        m_OptionScreen.SetActive(true);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Scenes/Game");
+    }
+    
+}
